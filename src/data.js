cc9ddb4680d2960790e8c68e9b966e2e5156e716
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-16 21:44:35 +0300

    add

--- src/data.js
+++ src/data.js
@@ -1,67 +1,53 @@
 export const questions = [
     {
-        title: 'Who became champion in 2017?',
-        variants: ['Hamilton', 'Verstappen', 'Fettel', 'Peres'],
+        title: 'Name the turn where MAX and LEWIS collide',
+        variants: ['Eau Rouge', 'Parabolica', 'Copse', 'La Source'],
+        correct: 2,
+    },
+    {
+        title: 'How many NIGHT RACES in F1?',
+        variants: ['4', '5', '3', '2'],
         correct: 0,
     },
     {
-        title: 'How many seconds did the fastest pit stop in F1 last?',
-        variants: ['2.04s', '2.12s', '1.92s', '1.98s'],
+        title: 'Who have NEVER RACED in F1?',
+        variants: ['Michael Schumacher', 'David Schumacher', 'Mick Schumacher', 'Ralf Schumacher'],
+        correct: 1,
+    },
+    {
+        title: 'Who is the YOUNGEST F1 driver to WIN a race?',
+        variants: ['Fernando Alonso', 'Lewis Hamilton', 'Max Verstappen', 'Michael Schumacher'],
         correct: 2,
     },
     {
-        title: 'How many races were there in the 2017 season?',
-        variants: ['18', '20', '17', '21'],
+        title: 'The SHORTEST track in F1 2022 calendar is...',
+        variants: ['Red Bull Ring (Austria)', 'Circuit de Monaco', 'Zandvoort (The Netherlands)', 'Interlagos (Brazil)'],
         correct: 1,
     },
     {
-        title: 'What’s the name of the Formula One curve where cars change course 180 degrees?',
-        variants: ['chicane', 'Apeks', 'Undercut', 'Spike'],
-        correct: 3,
+        title: 'The LONGEST F1 circuit on 2022 calendar is...',
+        variants: ['Jeddah Street Circuit', 'Monza', 'Spa-Franchorchamps', 'Circuit Gilles Villeneuve (Canada)'],
+        correct: 2,
     },
     {
-        title: 'How many wins does Sebastian Vettel have in 2017?',
-        variants: ['6', '5', '4', '3'],
-        correct: 1,
+        title: 'The HIGHEST speed of F1 EVER recorded is...',
+        variants: ['368 km/h', '355 km/h', '372 km/h', '380 km/h'],
+        correct: 2,
     },
     {
-        title: 'How much does a pilot lose in weight during a Grand Prix?',
-        variants: ['1.5', '2', '3.4', '5'],
-        correct: 1,
-    },
-    {
-        title: 'What’s the average temperature in the cockpit?',
-        variants: ['50deg', '45deg', '60deg', '35deg'],
+        title: 'In which TEAM LEWIS HAMILTON won his FIRST world title?',
+        variants: ['McLaren', 'Mercedes AMG', 'Brawn GP', 'Renault'],
         correct: 0,
     },
     {
-        title: 'How many women have raced in history?',
-        variants: ['10', '15', '5', '24'],
+        title: 'What is the BIGGEST AVERAGE speed F1 circuit?',
+        variants: ['Silverstone', 'Spa-Franchorchamps', 'Monza', 'Jeddah Street Circuit'],
         correct: 2,
     },
     {
-        title: 'To fill cars use helicopter equipment. And what is the speed of refueling?',
-        variants: ['17', '20', '30', '12'],
-        correct: 3,
-    },
-    {
-        title: 'Which of the champions of Formula 1 learned to race «Lada»?',
-        variants: ['Mark Webber', 'Louis Hammilton', 'Kimi Raikkonen', 'Nico Rosberg'],
+        title: 'How many CAR MANUFACTURERS TEAMS in F1?',
+        variants: ['5', '7', '6', '4'],
         correct: 2,
     },
-    {
-        title: 'Formula 1 is a very dangerous sport. Most affected is this team:',
-        variants: ['Ferrari', 'Caterham', 'Sauber', 'Force India'],
-        correct: 0,
-    },
-    {
-        title: 'Formula 1 riders - superstitious people in the peloton will never find a car under the number:',
-        variants: ['0', '13', '66', '3'],
-        correct: 1,
-    },
-    {
-        title: 'Superstitious not only riders: mechanics believe that during a two-hour race in no way can:',
-        variants: ['Talk', 'Come bolid around the front', 'To wash their hands', 'Joke'],
-        correct: 2,
-    },
+
 ];
